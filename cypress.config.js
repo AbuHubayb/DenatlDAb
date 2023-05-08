3743b7f335aa4f4c51734d5be7af5813089d4acc
AbuHubayb <67653660+AbuHubayb@users.noreply.github.com>
2023-05-08 18:35:20 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://ddstaging.byteproducts.com',
     chromeWebSecurity: false,
-    viewportHeight: 1000,
-    viewportWidth: 1600,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
